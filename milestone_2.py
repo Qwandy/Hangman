f1bd1587e9ec8007b96d75d4ba05118cf3b65ec4
Qwandy <sarunas.slepetis@hotmail.com>
2023-11-21 17:42:49 +0000

    Adds a conditional check to the guess variable to make sure it is only one character

--- milestone_2.py
+++ milestone_2.py
@@ -8,6 +8,6 @@ word = random.choice(word_list)
 guess = str(input("Enter a letter you think may be in the random word"))
 
 if len(guess) == 1:
-    print("Thank you for your guess")
+    print("Good guess!")
 else:
-    print("Your guess is not valid, guess again but make sure it is one character")
+    print("Oops! That is not a valid input.")
