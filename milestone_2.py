cbcf245c3bd7f0cdf0898cc8b9570709163ced37
Qwandy <sarunas.slepetis@hotmail.com>
2023-11-21 17:55:56 +0000

    Restructures the code so that it is within a 'Hangman' class, and makes some function calls to initialise the game

--- milestone_2.py
+++ milestone_2.py
@@ -1,13 +1,27 @@
 import random
-
 word_list = ['banana', 'strawberry', 'grape', 'blackberry', 'blackcurrant']
 
-word = random.choice(word_list)
+class Hangman():
+   
+    def __init__(self, word_list):
+        self.word_list = word_list
+
+    def generate_random_word(self):
+        word = random.choice(word_list)
+        return word
+    
+    def take_user_guess(self):
+        guess = str(input("Enter a letter you think may be in the random word"))
+
+        if len(guess) == 1:
+            print("Good guess!")
+            return guess
+        else:
+            print("Oops! That is not a valid input.")
+            guess = str(input("Enter a letter you think may be in the random word"))
+
+game_1 = Hangman(word_list)
+secret_word = game_1.generate_random_word()
+first_guess = game_1.take_user_guess()
 
 
-guess = str(input("Enter a letter you think may be in the random word"))
-
-if len(guess) == 1:
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
