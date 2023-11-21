bc13aad422d938fd27d7a3e1191e5c8ce11b3693
Qwandy <sarunas.slepetis@hotmail.com>
2023-11-21 17:36:55 +0000

    Allows the user to guess a letter and stores it in the guess variable

--- milestone_2.py
+++ milestone_2.py
@@ -2,9 +2,6 @@ import random
 
 word_list = ['banana', 'strawberry', 'grape', 'blackberry', 'blackcurrant']
 
+word = random.choice(word_list)
 
-for i in range(5):
-    word = random.choice(word_list)
-    print(word)
-    
-print(word_list)
+guess = str(input("Enter a letter you think may be in the random word"))
