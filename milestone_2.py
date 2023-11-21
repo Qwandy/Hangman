a8f853a5fd0338a226d883c44fef9818898a2b82
Qwandy <sarunas.slepetis@hotmail.com>
2023-11-21 17:33:12 +0000

    Implements the random.choice functionality to come up with a random word from the list and uses it in a loop to test it printing random words from the list

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,10 @@
+import random
+
 word_list = ['banana', 'strawberry', 'grape', 'blackberry', 'blackcurrant']
 
+
+for i in range(5):
+    word = random.choice(word_list)
+    print(word)
+    
 print(word_list)
