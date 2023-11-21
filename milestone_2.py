6d9b5398c2e542cd8305242928410710c1bc270a
Qwandy <sarunas.slepetis@hotmail.com>
2023-11-21 17:41:18 +0000

    Adds a conditional check to the guess variable to make sure it is only one character

--- milestone_2.py
+++ milestone_2.py
@@ -4,4 +4,10 @@ word_list = ['banana', 'strawberry', 'grape', 'blackberry', 'blackcurrant']
 
 word = random.choice(word_list)
 
+
 guess = str(input("Enter a letter you think may be in the random word"))
+
+if len(guess) == 1:
+    print("Thank you for your guess")
+else:
+    print("Your guess is not valid, guess again but make sure it is one character")
